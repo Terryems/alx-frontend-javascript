b49ad9f224b19e341cfc597c918b89a6f6efd46a
Terryems <emmanuelsimona@gmail.com>
2023-10-06 21:42:29 +0000

    new

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,1 +1,44 @@
-task_0/js/
+task_0/jsinteriface Student {
+    firstName: string,
+    lastName: string,
+    age: number,
+    location: string
+}
+
+const firstStudent : Student ={
+    firstName: 'Simon',
+    lastName: 'ems',
+    age: 30,
+    location: 'Nigeria'
+}
+
+const secondStudent : Student = {
+    firstName: 'Emma',
+    lastName: 'Nuel',
+    age: 34,
+    location: 'Nigeria'
+}
+
+const studentsList: Student[] = [firstStudent, secondStudent];
+
+
+function renderTable() {
+    const table = document.createElement('table');
+  
+    studentsList.forEach((student) => {
+      const row = document.createElement('tr');
+      const firstNameCell = document.createElement('td');
+      const locationCell = document.createElement('td');
+  
+      firstNameCell.textContent = student.firstName;
+      locationCell.textContent = student.location;
+  
+      row.appendChild(firstNameCell);
+      row.appendChild(locationCell);
+  
+      table.appendChild(row);
+    });
+    document.body.appendChild(table);
+}
+
+renderTable();/
